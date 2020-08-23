eea8ffeb5019cb146f5585a54f497f55c8fa8307
Satej Bidvai <satej.bidvai@gmail.com>
2020-08-23 21:57:16 +0530

    Dashboard Component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
+import Dashboard from './components/dashboard/Dashboard';
 
 const App = () => {
     return (
         <>
             <Navbar />
             <Switch>
-                <Route path="/" exact component={() => <h1>Home Page</h1>} />
+                <Route path="/" exact component={() => <Dashboard />} />
             </Switch>
         </>
     );
