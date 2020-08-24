756a706d8344662d99445e2b861b85da9c303ece
Satej Bidvai <satej.bidvai@gmail.com>
2020-08-24 15:08:15 +0530

    Redux initial setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from 'react-router-dom'
-
+import { BrowserRouter as Router } from 'react-router-dom';
+import { createStore } from 'redux';
 import App from './App';
+import rootReducer from './store/reducers/rootReducer';
+import { Provider } from 'react-redux';
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
-    <Router>
-        <App />
-    </Router>,
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>,
     document.getElementById('root')
 );
