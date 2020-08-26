23abead5300b4caf453b964fd8d7fcb1fcf602af
Satej Bidvai <satej.bidvai@gmail.com>
2020-08-26 14:17:48 +0530

    Create Project notification added

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,33 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    functions.logger.info('Hello logs!', { structuredData: true });
+    response.send('Hello Firebase!');
+});
+
+const createNotification = (notification) => {
+    return admin
+        .firestore()
+        .collection('notifications')
+        .add(notification)
+        .then((doc) => {
+            console.log('Notification Added!', doc);
+        });
+};
+
+exports.projectCreated = functions.firestore
+    .document('projects/{projectId}')
+    .onCreate((doc) => {
+        const project = doc.data();
+
+        const notification = {
+            content: 'New Project Added!',
+            user: `${project.firstName} ${project.lastName}`,
+            time: admin.firestore.FieldValue.serverTimestamp(),
+        };
+
+        return createNotification(notification);
+    });
