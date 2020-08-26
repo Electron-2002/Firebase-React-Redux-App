068cce1b49234d8aae203408ad401279c361df01
Satej Bidvai <satej.bidvai@gmail.com>
2020-08-26 16:40:19 +0530

    Deployed to Firebase

---
+++ dist/precache-manifest.18cc032a5a985da96df602afeda05066.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "116e93ceb863f1b6d50d65d37b9102cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16b64aadafabe8f39f29",
+    "url": "/static/css/main.c98fd039.chunk.css"
+  },
+  {
+    "revision": "0a4d8f349d8ccc8853b4",
+    "url": "/static/js/2.4d59acbf.chunk.js"
+  },
+  {
+    "revision": "d56f7d70e49a582c3cb88e84fa36367e",
+    "url": "/static/js/2.4d59acbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16b64aadafabe8f39f29",
+    "url": "/static/js/main.c86dbbe5.chunk.js"
+  },
+  {
+    "revision": "66b327f18306c15d79d9",
+    "url": "/static/js/runtime-main.28cf7c2b.js"
+  }
+]);
